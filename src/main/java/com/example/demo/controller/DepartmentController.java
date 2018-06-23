2887613cbba7e6ac103db023b58676944f37c6f9
Jesse <lijixiang@qtongx.com>
2018-06-24 01:16:15 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import com.example.demo.mapper.DepartmentMapper;
+import com.example.demo.model.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class DepartmentController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    DepartmentMapper departmentMapper;
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Map<String,Object> map(){
+        List<Map<String,Object>> list = jdbcTemplate.queryForList("select * from department");
+        return list.get(0);
+    }
+
+    /**根据部门ID查询部门**/
+    @ResponseBody
+    @GetMapping("/department/{id}")
+    public ModelMap findDeptById(@PathVariable int id){
+        ModelMap mv = new ModelMap();
+        Department department = departmentMapper.getDeptById(id);
+        mv.addAttribute("dept",department);
+        return mv;
+    }
+
+    /**查询所有部门信息**/
+    @ResponseBody
+    @GetMapping("/findAll")
+    public Map<String,Object> findAll(){
+        List<Department> list = departmentMapper.getAllDept();
+        Map<String,Object> map = new HashMap<>();
+        map.put("findAll",list);
+        return map;
+    }
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloJenkins(){
+        return "Hello Jenkins";
+    }
+}
---
+++ src/test/java/com/example/demo/LoggerTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggerTest {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+    @Test
+    public void contextLoads() {
+        //日志的级别,由低到高   trace<debug<info<warn<error
+        logger.trace("trace...");
+        logger.debug("debug...");
+        logger.info("info...");
+        logger.warn("warn...");
+        logger.error("error...");
+    }
+}
